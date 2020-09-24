dfa22f6c1a6b10f265a73ec809e9e74008e6d36f
Alex <allexxx81@gmail.com>
2020-09-24 15:31:24 +0200

    erste Endvariante

---
+++ Fahrstuhl/FahrsthuhlClass.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fahrstuhl
+{
+
+    enum Etagen { erdgeschoss, erste, zweite, dritte ,vierte}
+
+
+    class FahrsthuhlClass
+    {
+        public delegate void StartHandler(int e);
+        public event StartHandler Start;
+        public bool Knopf { get; set; }
+        public bool NachOben { get; set; }
+        public int AnzahlEtagen { get; set; }
+        public Etagen aufenthalt { get; set; } = Etagen.erdgeschoss;
+
+        public List<Etagen> EtagenList = new List<Etagen>(){Etagen.erdgeschoss, Etagen.erste, Etagen.zweite, Etagen.dritte, Etagen.vierte};
+
+        public FahrsthuhlClass()
+        {
+        }
+
+        public void KnopfGedrueckt(int e)
+        {
+            Start += Fahren;
+            if ((int)aufenthalt < e)
+            {
+                NachOben = true;
+            }
+            else
+            {
+                NachOben = false;
+            }
+
+
+            Start?.Invoke(e);
+        }
+
+
+        public void Halten ()
+        {
+            Start -= Fahren;
+        }
+        public void Fahren(int e)
+        {
+
+            int i = (int)aufenthalt;
+            while (NachOben ? i < e : i >= e)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(EtagenList[i]);
+                aufenthalt = EtagenList[e];
+
+                 _= NachOben ? i++ : i--;
+            }
+            Halten();
+            Console.WriteLine("Die Tür wird geöffnet");
+        }
+    }
+}
---
+++ Fahrstuhl/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrstuhl
+{
+    class Program
+    {
+        /*
+             Baut einen Fahrstuhl
+
+            Der Fahrstuhl bedient 5 Etagen (Erdgeschoss, 1,2,3,4)
+            Auf jeder Etage kann ein potentieller Fahrgast auf einen Knopf drücken
+            Es gibt nur einen Knopf, der dem Fahrstuhl signaliert, dass da jemand wartet.
+            Egal, wo der Fahrgast hin möchte ... Der Fahrstuhl behält seine Richtung bei.
+
+            Beispiel: Im Erdgeschoss steigt jemand ein und will in die 4 Etage
+            In der 2. Etage steigt jemand ein und will ins Erdgeschoss
+            Der Fahrstuhl fährt erst in die 4. Etage, dann wieder runter.
+
+            Während der Fahrstuhl vor sich hin fahrstuhlt, können weitere Gäste auf beliebigen
+            Etagen ankommen und auf den Knopf drücken. Diese neuen Gäste sollen auch
+            berücksichtigt werden.
+         */
+        static void Main(string[] args)
+        {
+            
+            FahrsthuhlClass fahrsthuhl1 = new FahrsthuhlClass();
+
+            fahrsthuhl1.KnopfGedrueckt(3);
+            //fahrsthuhl1.KnopfGedrueckt(3);
+            fahrsthuhl1.KnopfGedrueckt(4);
+            fahrsthuhl1.KnopfGedrueckt(0);
+        }
+    }
+}
